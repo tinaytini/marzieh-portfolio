4ca74f76a19ed96582acea966fb0c612bcf4f9f2
Tinatin <tinaytini@yahoo.com>
2024-11-01 14:37:53 +0100

    portfolio

--- src/index.js
+++ src/index.js
@@ -1,6 +1,55 @@
 import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
+import { projects } from "./projects";
 
+
+
+
+const projectList = document.getElementById("#project__list")
+
+projects.forEach(item => {
+    const rowDiv = document.createElement('div');
+    rowDiv.className = 'row';
+
+    const cardDiv = document.createElement('div');
+    cardDiv.className = 'card';
+
+    const textDiv = document.createElement('div');
+    textDiv.className = 'project-wrapper__text load-hidden';
+
+    const title = document.createElement('h3');
+    title.className = 'project-wrapper__text-title';
+    title.textContent = item.title;
+
+    const imageDiv = document.createElement('div');
+    imageDiv.className = 'project-wrapper__image load-hidden';
+
+    const link = document.createElement('a');
+    link.rel = 'noreferrer';
+    link.href = item.source; // Set the actual URL from the project array
+    link.target = '_blank';
+
+    const thumbnailDiv = document.createElement('div');
+    thumbnailDiv.className = 'thumbnail rounded js-tilt';
+    thumbnailDiv.setAttribute('data-tilt', '');
+    thumbnailDiv.setAttribute('data-tilt-max', '4');
+    thumbnailDiv.setAttribute('data-tilt-glare', 'true');
+    thumbnailDiv.setAttribute('data-tilt-max-glare', '0.5');
+
+    const img = document.createElement('img');
+    img.className = 'img-fluid';
+    img.src = item.image;
+    img.alt = 'Project Image';
+
+    thumbnailDiv.appendChild(img);
+    link.appendChild(thumbnailDiv);
+    imageDiv.appendChild(link);
+    textDiv.appendChild(title);
+    textDiv.appendChild(imageDiv);
+    cardDiv.appendChild(textDiv);
+    rowDiv.appendChild(cardDiv);
+    projectList.appendChild(rowDiv);
+});
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
---
+++ src/projects.js
@@ -0,0 +1,64 @@
+import projectImage from './assets/project.jpg';
+
+export const projects = [
+    {
+        title: 'Fruad detection in Crypto currency',
+        image: projectImage,
+        source: 'https://github.com/Mari-kn/Fraud_Detection_Crypto_Python',
+    },
+    {
+        title: 'Salary prediction',
+        image: projectImage,
+        source: 'https://github.com/Mari-kn/Salary_prediction_Python',
+    },
+    {
+        title: 'Cost prediction',
+        image: projectImage,
+        source: 'https://github.com/Mari-kn/Cost_prediction_python',
+    },
+    {
+        title: 'Stock prediction sentiment analysis',
+        image: projectImage,
+        source: 'https://github.com/Mari-kn/Stock_Prediction_Sentiment_Huggingface',
+    },
+    {
+        title: 'Web Scrap',
+        image: projectImage,
+        source: 'https://github.com/Mari-kn/WebScrap_TabletoDataFrame_Python',
+    },
+    {
+        title: 'Customer churn prediction',
+        image: projectImage,
+        source: 'https://github.com/Mari-kn/Customer_Churn_Prediction_Imbalenced_Data',
+    },
+    {
+        title: 'HIV EDA',
+        image: projectImage,
+        source: 'https://github.com/Mari-kn/HIV_Analytics_Python',
+    },
+    {
+        title: 'Gold News analysis',
+        image: projectImage,
+        source: 'https://github.com/Mari-kn/API_News_Analytics_Python',
+    },
+    {
+        title: 'Energy consumption project',
+        image: projectImage,
+        source: 'https://github.com/Mari-kn/Energy_Consumption_Prediction_Python',
+    },
+    {
+        title: 'Call center GenAI',
+        image: projectImage,
+        source: 'https://github.com/Mari-kn/call-center-GenAI',
+    },
+    {
+        title: 'Chatbot GenAI',
+        image: projectImage,
+        source: 'https://github.com/Mari-kn/Chatbot_OpenAI',
+    },
+    {
+        title: 'Kids story telling',
+        image: projectImage,
+        source: 'https://github.com/Mari-kn/Kids-Story-OpenAI',
+    }
+]
